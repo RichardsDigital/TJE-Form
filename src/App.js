96f5a8b927559dbe9fcf9c52b1f2aea19027f72e
Green Creative <greencreative@Greens-iMac-85.local>
2020-08-18 16:46:17 +0100

    Removed useRef

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect} from 'react';
 import './styles/global.scss';
 import logo from './images/TJE.png';
 import StandardInput from './components/labels/StandardInput';
